276716fcdd02e3b2c6a2110bc67ea589fef7b00f
boshna1 <joshua.dinata@ontariotechu.net>
2025-03-30 14:30:35 -0400

    trying again to fix solution

--- Room.cpp
+++ Room.cpp
@@ -3,22 +3,26 @@
 #include <Room.h>
 #include <iostream>
 
-void Room :: AddEnemy(std::unique_ptr<Enemy> enemy) {
-	
-	enemies.push_back(std:: move(enemy));
 
-}
-void Room::AddLoot(const std:: string& item) {
+namespace RoomC
+{
+	void Room::AddEnemy(std::unique_ptr<Enemy> enemy) {
 
-	loot.push_back(item);
-}
-void Room::AddAdjacentRoom(int roomNum) {
+		enemies.push_back(std::move(enemy));
 
-	adjacentRooms.push_back(roomNum);
-}
-void Room::DisplayRoomInfo() {
+	}
+	void Room::AddLoot(const std::string& item) {
 
-	cout << "Room: " << roomNumber << "\n";
-	cout << "Enemies: " << enemies.size() << "\n";
+		loot.push_back(item);
+	}
+	void Room::AddAdjacentRoom(int roomNum) {
 
+		adjacentRooms.push_back(roomNum);
+	}
+	void Room::DisplayRoomInfo() {
+
+		cout << "Room: " << roomNumber << "\n";
+		cout << "Enemies: " << enemies.size() << "\n";
+
+	}
 }
