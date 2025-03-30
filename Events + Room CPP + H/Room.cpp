0ead6ba19330fad3d041aec651af5d3a32a36fa2
laithooo <123286051+laithooo@users.noreply.github.com>
2025-03-30 13:50:31 -0400

    Added my part
    Events + Room code

---
+++ Events + Room CPP + H/Room.cpp
@@ -0,0 +1,24 @@
+
+
+#include <Room.h>
+#include <iostream>
+
+void Room :: AddEnemy(std::unique_ptr<Enemy> enemy) {
+	
+	enemies.push_back(std:: move(enemy));
+
+}
+void Room::AddLoot(const std:: string& item) {
+
+	loot.push_back(item);
+}
+void Room::AddAdjacentRoom(int roomNum) {
+
+	adjacentRooms.push_back(roomNum);
+}
+void Room::DisplayRoomInfo() {
+
+	cout << "Room: " << roomNumber << "\n";
+	cout << "Enemies: " << enemies.size() << "\n";
+
+}
